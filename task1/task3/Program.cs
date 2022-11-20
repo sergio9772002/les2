eaddaa225167540ed908fd58f7eb6cef1bfc7d19
Sergio <sergio977@rambler.ru>
2022-11-20 16:36:55 +0400

    add task3 new

--- task1/task3/Program.cs
+++ task1/task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число и выдает, является ли число
+четным (делится ли оно на два без остатка).
+*/
+int userNumber1 = new int();
+Console.WriteLine("Введите число");
+userNumber1 = Convert.ToInt32(Console.ReadLine());
+if (userNumber1 % 2 ==0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
