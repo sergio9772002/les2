9efae43a8237d0a0b15cb326029e5b6b7590bdfc
Sergio <sergio977@rambler.ru>
2022-11-20 16:27:06 +0400

    add task2

--- task1/task2/Program.cs
+++ task1/task2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход три числа и выдает максимальное
+из этих чисел.
+*/
+int userNumber1 = new int();
+int userNumber2 = new int();
+int userNumber3 = new int();
+Console.WriteLine("Введите число");
+userNumber1 = Convert.ToInt32(Console.ReadLine());
+userNumber2 = Convert.ToInt32(Console.ReadLine());
+userNumber3 = Convert.ToInt32(Console.ReadLine());
+if (userNumber1 > userNumber2)
+{
+    if (userNumber1 > userNumber3)
+    {
+      Console.Write("-> ");
+      Console.Write(userNumber1);  
+    }
+    else
+    {
+      Console.Write("-> ");
+      Console.Write(userNumber3);
+    }
+}
+else
+{
+   if (userNumber2 > userNumber3) 
+   {
+      Console.Write("-> ");
+      Console.Write(userNumber2);
+   }
+   else
+   {
+      Console.Write("-> ");
+      Console.Write(userNumber3);
+   }
+}
